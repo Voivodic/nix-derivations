babd633d703db2c0640b636ac19095a032b93538
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2025-04-22 18:56:51 -0300

    Moved the package.nix to package/default.nix

--- flake.nix
+++ flake.nix
@@ -14,9 +14,9 @@
         unstable = import nixpkgs-unstable { system = "${system}"; };
 
         # Call the packages in the Repo
-        pyexshalos = unstable.python313Packages.callPackage ./pkgs/cosmo/pyexshalos.nix {};
-        e3nn-jax = unstable.python313Packages.callPackage ./pkgs/nn/e3nn_jax.nix {};
-        diffrax = unstable.python313Packages.callPackage ./pkgs/nn/diffrax.nix {};
+        pyexshalos = unstable.python313Packages.callPackage ./pkgs/cosmo/pyexshalos {};
+        e3nn-jax = unstable.python313Packages.callPackage ./pkgs/nn/e3nn_jax {};
+        diffrax = unstable.python313Packages.callPackage ./pkgs/nn/diffrax {};
     in { 
         devShells.${system} = {
             pyexshalos = stable.mkShell {
