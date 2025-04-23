1964176bbaa0f084effbce72a69c35459e530053
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2025-04-23 02:37:33 -0300

    New main flake.nix

--- flake.nix
+++ flake.nix
@@ -1,39 +1,30 @@
 {
-    description = "Test of the derivations in this folder";
+    description = "Voivodic's custom packages for nix";
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-        nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, nixpkgs-unstable, ... }: 
-    let
-        # Define the system
-        system = "x86_64-linux";
-        stable = import nixpkgs { system = "${system}"; };
-        unstable = import nixpkgs-unstable { system = "${system}"; };
+    outputs = { self, nixpkgs, flake-utils, ... }:
+        flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };
+        
+            # Package definitions
+            packages = {
+                # Cosmo
+                pyexshalos = pkgs.python3Packages.callPackage ./pkgs/cosmo/pyexshalos {};
 
-        # Call the packages in the Repo
-        pyexshalos = unstable.python313Packages.callPackage ./pkgs/cosmo/pyexshalos {};
-        e3nn-jax = unstable.python313Packages.callPackage ./pkgs/nn/e3nn_jax {};
-        diffrax = unstable.python313Packages.callPackage ./pkgs/nn/diffrax {};
-    in { 
-        devShells.${system} = {
-            pyexshalos = stable.mkShell {
-                buildInputs = [
-                    pyexshalos
-                ];
+                # NN
+                e3nn-jax = pkgs.python3Packages.callPackage ./pkgs/nn/e3nn-jax {};
+                diffrax = pkgs.python3Packages.callPackage ./pkgs/nn/diffrax {};
             };
-            e3nn-jax = stable.mkShell {
-                buildInputs = [
-                    e3nn-jax
-                ];
-            };
-            diffrax = stable.mkShell {
-                buildInputs = [
-                    diffrax 
-                ];
-            };
-        };
-    };
+        in {
+            packages = packages;
+           
+            # Expose as overlay
+            overlays.default = final: prev: packages;
+        }
+    );
 }
