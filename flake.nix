4fd36dc28daeb896cf95a1b981230d2da5b4e47a
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2025-05-30 13:32:59 -0300

    Added option to use python 3.12 or 3.13 for python codes

--- flake.nix
+++ flake.nix
@@ -8,27 +8,42 @@
 
     outputs = { self, nixpkgs, flake-utils, ... }:
         flake-utils.lib.eachDefaultSystem (system:
-        let
-            pkgs = import nixpkgs { inherit system; };
-        
-            # Package definitions
-            packages = {
-                # Cosmo
-                pyexshalos = pkgs.python3Packages.callPackage ./pkgs/cosmo/pyexshalos {};
-                class-pt = pkgs.python3Packages.callPackage ./pkgs/cosmo/class-pt {};
+            let
+                pkgs = import nixpkgs { inherit system; };
 
-                # NN
-                e3nn-jax = pkgs.python3Packages.callPackage ./pkgs/nn/e3nn-jax {};
-                diffrax = pkgs.python3Packages.callPackage ./pkgs/nn/diffrax {};
+                # Package definitions
+                packages = {
+                    python312 = {
+                        # Cosmo
+                        pyexshalos = pkgs.python312Packages.callPackage ./pkgs/cosmo/pyexshalos {};
+                        class-pt = pkgs.python312Packages.callPackage ./pkgs/cosmo/class-pt {};
 
-                # Utils
-                getdist = pkgs.python3Packages.callPackage ./pkgs/utils/getdist {};
-            };
-        in {
-            packages = packages;
-           
-            # Expose as overlay
-            overlays.default = final: prev: packages;
-        }
-    );
+                        # NN
+                        e3nn-jax = pkgs.python312Packages.callPackage ./pkgs/nn/e3nn-jax {};
+                        diffrax = pkgs.python312Packages.callPackage ./pkgs/nn/diffrax {};
+
+                        # Utils
+                        getdist = pkgs.python312Packages.callPackage ./pkgs/utils/getdist {};
+                    };
+                    python313 = {
+                        # Cosmo
+                        pyexshalos = pkgs.python313Packages.callPackage ./pkgs/cosmo/pyexshalos {};
+                        class-pt = pkgs.python313Packages.callPackage ./pkgs/cosmo/class-pt {};
+
+                        # NN
+                        e3nn-jax = pkgs.python313Packages.callPackage ./pkgs/nn/e3nn-jax {};
+                        diffrax = pkgs.python313Packages.callPackage ./pkgs/nn/diffrax {};
+
+                        # Utils
+                        getdist = pkgs.python313Packages.callPackage ./pkgs/utils/getdist {};
+                    };
+
+                };
+            in {
+                packages = packages;
+
+                # Expose as overlay
+                overlays.default = final: prev: packages;
+            }
+        );
 }
