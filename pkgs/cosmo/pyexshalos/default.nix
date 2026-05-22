42c70fdb47c6327d6502b5ba599a96fccbd01f39
Rodrigo Voivodic <rodrigo.voivodic@gmail.com>
2026-05-21 19:29:01 -0500

    Derivation for pyexshalos changed to remove voro.cpp (not used in this version yet).

--- pkgs/cosmo/pyexshalos/default.nix
+++ pkgs/cosmo/pyexshalos/default.nix
@@ -19,48 +19,17 @@
     numpy,
     scipy,
 }: 
-let 
-    # Derivation for voro++ (used in ExSHalos) 
-    voroPP = stdenv.mkDerivation { 
-        pname = "voro++"; 
-        version = "0.5"; 
-
-        src = fetchFromGitHub{ 
-            owner = "chr1shr"; 
-            repo = "voro"; 
-            rev = "master"; 
-            sha256 = "sha256-F6KersvAa/LKEFjfolGWPjxlsmfFh5F/cV2CrmO8ntA="; 
-        }; 
-
-        nativeBuildInputs = [
-            gcc
-        ]; 
-
-        buildPhase = '' 
-            make CFLAGS="$CFLAGS -Wall -ansi -pedantic -O3 -fPIC" 
-        ''; 
-
-        installPhase = '' 
-            make install PREFIX=$out
-        ''; 
-
-        meta = { 
-            description = "A three-dimensional Voronoi cell library in C++"; 
-            homepage = "https://math.lbl.gov/voro++/"; 
-        }; 
-    }; 
-in 
 # Derivation for pyexshalos 
 buildPythonPackage rec { 
     pname = "pyexshalos"; 
-    version = "0.1.0"; 
+    version = "1.0.0"; 
     format = "setuptools"; 
 
     src = fetchFromGitHub{ 
         owner = "Voivodic"; 
-        repo = "ExSHalos"; 
+        repo = "exshalos"; 
         tag = "v${version}";
-        sha256 = "sha256-F7khUGrD7sdlz3YIABxf4wrOuL8eww0NCIdmNRF4+mY="; 
+        sha256 = "sha256-Mi60FWRop8FoZGLH2IXf8wi2cNPlzh2+gqAtZFLEQhM="; 
     }; 
 
     nativeBuildInputs = [
@@ -71,7 +40,6 @@ buildPythonPackage rec {
         fftw 
         fftwFloat 
         gsl 
-        voroPP 
         setuptools
     ]; 
 
